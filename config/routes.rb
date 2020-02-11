692163350509fa784807a224d8c2a9221b417892
sourabhk2020 <sourabh@alohatechnologydev.com>
2020-02-11 15:12:46 +0530

    created new template, action and route for adding new article

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   #get 'pages/home', to: 'pages#home'
   #get 'pages/about', to: 'pages#about'
   get 'about', to: 'pages#about'
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
