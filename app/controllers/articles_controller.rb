590dc656cf32633e1d656d4437e59b86b7c881a0
sourabhk2020 <sourabh@alohatechnologydev.com>
2020-02-11 21:09:33 +0530

    destroy, partials functionality added and home to blog and blog home link added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,13 @@ class ArticlesController < ApplicationController
       render "edit"
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article successfully deleted!"
+    redirect_to articles_path
+  end
   
   private
     def article_params
