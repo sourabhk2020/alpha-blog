f291b34dda61a11d5fd57da323d9d36d47a5acc1
sourabhk2020 <sourabh@alohatechnologydev.com>
2020-02-11 19:55:45 +0530

    Add, edit, update and index listing page created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,47 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article created successfully...!"
+      #redirect_to @article
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+    # @article.save
+    # redirect_to articles_path(@article)
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully...!"
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
+
 end
