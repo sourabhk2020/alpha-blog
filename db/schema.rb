c2af33c9f2a11f5dd5050e54456d55567e00c72b
sourabhk2020 <sourabh@alohatechnology.com>
2020-02-12 21:17:22 +0530

    destroy, partial functionality and before_action wrap functionality has been added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_132742) do
+ActiveRecord::Schema.define(version: 2020_02_12_141122) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text "article_type"
   end
 
 end
