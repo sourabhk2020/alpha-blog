1596cda5011a57c3fbe1a56d723499f1640e13fb
sourabhk2020 <sourabh@alohatechnology.com>
2020-02-14 13:36:20 +0530

    create users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_141122) do
+ActiveRecord::Schema.define(version: 2020_02_14_074752) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_02_12_141122) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "article_type"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
