c6374acfc68d2ae1a1124273a53cd4ca719ae7c3
sourabhk2020 <sourabh@alohatechnology.com>
2020-02-17 16:09:33 +0530

    added secure password, add user, edit user, user gravatar and partials in articles and users folder

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   #get 'pages/about', to: 'pages#about'
   get 'about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_091644) do
+ActiveRecord::Schema.define(version: 2020_02_14_113646) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_091644) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
