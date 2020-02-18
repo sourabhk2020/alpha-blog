2e03eaf358c2028adad9cf42512d4ade6d282dc1
sourabhk2020 <sourabh@alohatechnology.com>
2020-02-18 13:15:22 +0530

    restrict actions from ui and controller, sign in upon signup functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
+
     def index
       #@users = User.all
       @users = User.paginate(page: params[:page], per_page: 5)
@@ -12,19 +15,20 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:success] = "Welcome to the alpha-blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to user_path(@user)
       else 
         render "new"
       end
     end
 
     def edit
-      @user = User.find(params[:id])
+      #@user = User.find(params[:id])
     end
 
     def update
-      @user = User.find(params[:id])
+      #@user = User.find(params[:id])
       if @user.update(user_params)
         flash[:success] = "User account was updated successfully!"
         redirect_to articles_path
@@ -34,7 +38,7 @@ class UsersController < ApplicationController
     end
 
     def show
-      @user = User.find(params[:id])
+      #@user = User.find(params[:id])
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -42,6 +46,19 @@ class UsersController < ApplicationController
       
       def user_params
         params.require(:user).permit(:username, :email, :password)
+      end
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def require_same_user
+
+        if current_user != @user
+          flash[:danger] = "You can only edit your own account"
+          redirect_to root_path
+        end
+        
       end
 
 end
