313a559e130cef81fa9efd31d9cf01502cfcff1b
sourabhk2020 <sourabh@alohatechnology.com>
2020-02-18 21:28:18 +0530

    automation test added upto admin user requirement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_075907) do
+ActiveRecord::Schema.define(version: 2020_02_18_101312) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_02_18_075907) do
     t.datetime "updated_at"
     t.text "article_type"
     t.integer "user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
