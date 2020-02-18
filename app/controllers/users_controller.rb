b1f2f90d649b175638ea0887ac56e6710e7ee9db
sourabhk2020 <sourabh@alohatechnology.com>
2020-02-18 15:31:51 +0530

    Add admin user functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
 
     def index
       #@users = User.all
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
+    def destroy
+      @user.destroy
+      flash[:danger] = "User and all articles created by user have been deleted"
+      redirect_to users_path
+    end
+
     private
       
       def user_params
@@ -54,11 +61,18 @@ class UsersController < ApplicationController
 
       def require_same_user
 
-        if current_user != @user
+        if current_user != @user and !current_user.admin?
           flash[:danger] = "You can only edit your own account"
           redirect_to root_path
         end
         
+      end
+
+      def require_admin
+        if logged_in? and !current_user.admin?
+          flash[:danger] = "Only admin users can perform that action"
+          redirect_to root_path
+        end
       end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_113646) do
+ActiveRecord::Schema.define(version: 2020_02_18_075907) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_113646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
